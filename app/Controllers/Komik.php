b02b9406ed91b400e642cfeed605302cc613b1c9
Drajat Cahya Diningrat <71449473+DCD00@users.noreply.github.com>
2020-10-18 22:23:21 +0700

    Materi Model (Bagian 2)

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -14,11 +14,11 @@ class Komik extends BaseController
 
     public function index()
     {
-        $komik = $this->komikModel->findAll();
+        // $komik = $this->komikModel->findAll();
 
         $data = [
             'title' => 'Daftar Komik',
-            'komik' => $komik
+            'komik' => $this->komikModel->getKomik()
         ];
 
         // cara konek db tanpa model
@@ -30,5 +30,15 @@ class Komik extends BaseController
 
 
         return view('komik/index', $data);
+    }
+    
+    public function detail($slug)
+    {
+        $data = [
+            'title' => 'Detail Komik',
+            'komik' => $this->komikModel->getKomik($slug)
+        ];
+
+        return view('komik/detail', $data);
     }
 }
